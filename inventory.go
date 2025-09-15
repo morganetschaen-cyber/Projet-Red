ecc56c791c2ee7c629314386009053f2cb1d4e2b
tmorgane <morgane.tschaen@ynov.com>
2025-09-15 14:37:46 +0200

    tâche 4: correction accessInventory +inventaire vide

--- inventory.go
+++ inventory.go
@@ -2,20 +2,9 @@ package main
 
 import "fmt"
 
-func accessInventory(inventory map[string]int) {
+func accessInventory(inventory []string) {
 	if len(inventory) == 0 {
 		fmt.Println("Votre inventaire est vide.")
 		return
 	}
-
-	fmt.Println("Inventaire du personnage :")
-	for item, qty := range inventory {
-		fmt.Printf("- %s (x%d)\n", item, qty)
-	}
-}
-
-func main() {
-	inventory := map[string]int{}
-
-	accessInventory(inventory)
 }
