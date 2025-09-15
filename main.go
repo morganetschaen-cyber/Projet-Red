cb6929e211a28c780da42a1ba5e722793e55944b
Ivana Boycheva <ivana.boycheva@ynov.com>
2025-09-15 14:22:57 +0200

    Tache 2- initCharacter

--- main.go
+++ main.go
@@ -11,15 +11,20 @@ type Character struct {
 	Inventaire []string
 }
 
-func main() {
-	heros := Character{
-		Nom:        "Eva",
-		Classe:     "",
-		Niveau:     1,
-		PVMax:      100,
-		PVActuels:  50,
-		Inventaire: []string{"Potion", "Potion", "Potion"},
+func initCharacter(nom, classe string, niveau, pvMax, pvActuels int, inventaire []string) Character {
+	return Character{
+		Nom:        nom,
+		Classe:     classe,
+		Niveau:     niveau,
+		PVMax:      pvMax,
+		PVActuels:  pvActuels,
+		Inventaire: inventaire,
 	}
+}
+
+func main() {
+	inventaire := []string{"Potion", "Potion", "Potion"}
+	heros := initCharacter("Eva", "Elfe", 1, 100, 50, inventaire)
 
 	fmt.Println(heros)
 }
