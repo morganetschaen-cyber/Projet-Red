2761a373094cf23f27cd4b30ea12d040498331fa
tmorgane <morgane.tschaen@ynov.com>
2025-09-16 15:04:40 +0200

    tâche 15 suite: fonction craftItem

--- forgeron.go
+++ forgeron.go
@@ -5,42 +5,54 @@ import "fmt"
 func forgeron(c *Character) {
 	for {
 		fmt.Println("\n--- Forgeron ---")
-		fmt.Println("1. Chapeau de l’aventurier (5 pièces d'or)")
-		fmt.Println("2. Tunique de l’aventurier (5 pièces d'or)")
-		fmt.Println("3. Bottes de l’aventurier (5 pièces d'or)")
-		fmt.Println("4. Retour au menu")
+		fmt.Println("1. Chapeau de l’aventurier")
+		fmt.Println("2. Tunique de l’aventurier")
+		fmt.Println("3. Bottes de l’aventurier")
+		fmt.Println("4. Retour")
 
 		var choix int
 		fmt.Scan(&choix)
 
 		switch choix {
 		case 1:
-			if c.Argent >= 5 {
-				c.Argent -= 5
-				addInventory(c, "Chapeau de l’aventurier")
-			} else {
-				fmt.Println("Pas assez d'argent !")
+			if craftItem(c, "Chapeau de l’aventurier",
+				[]string{"Plume de Corbeau", "Cuir de Sanglier"}, 5) {
+				fmt.Println("Vous avez fabriqué un Chapeau de l’aventurier !")
 			}
 		case 2:
-			if c.Argent >= 5 {
-				c.Argent -= 5
-				addInventory(c, "Tunique de l’aventurier")
-			} else {
-				fmt.Println("Pas assez d'argent !")
+			if craftItem(c, "Tunique de l’aventurier",
+				[]string{"Fourrure de Loup", "Fourrure de Loup", "Peau de Troll"}, 5) {
+				fmt.Println("Vous avez fabriqué une Tunique de l’aventurier !")
 			}
 		case 3:
-			if c.Argent >= 5 {
-				c.Argent -= 5
-				addInventory(c, "Bottes de l’aventurier")
-			} else {
-				fmt.Println("Pas assez d'argent !")
+			if craftItem(c, "Bottes de l’aventurier",
+				[]string{"Fourrure de Loup", "Cuir de Sanglier"}, 5) {
+				fmt.Println("Vous avez fabriqué des Bottes de l’aventurier !")
 			}
 		case 4:
-			fmt.Println("Retour au menu principal.")
 			return
 		default:
 			fmt.Println("Choix invalide.")
 		}
-		fmt.Println("Argent actuel :", c.Argent)
+
 	}
+}
+
+func craftItem(c *Character, item string, ressources []string, prix int) bool {
+	if c.Argent < prix {
+		fmt.Println("Pas assez d’or.")
+		return false
+	}
+	for _, r := range ressources {
+		if !hasItem(c.Inventaire, r) {
+			fmt.Println("Ressource manquante :", r)
+			return false
+		}
+	}
+	for _, r := range ressources {
+		removeInventory(c, r)
+	}
+	c.Argent -= prix
+	addInventory(c, item)
+	return true
 }
