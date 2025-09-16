ef2f323acc0ffb89569ff3f856349909dd1bb1cc
tmorgane <morgane.tschaen@ynov.com>
2025-09-16 11:40:50 +0200

    tâche 10: ajout livre de sort boule de feu dans le marchand

--- shop.go
+++ shop.go
@@ -7,7 +7,8 @@ func marchand(c *Character) {
 		fmt.Println("\n--- Marchand ---")
 		fmt.Println("1. Potion de vie")
 		fmt.Println("2. Potion de poison")
-		fmt.Println("3. Retour au menu")
+		fmt.Println("3. Livre de Sort : Boule de Feu")
+		fmt.Println("4. Retour au menu")
 
 		var choix int
 		fmt.Scan(&choix)
@@ -18,6 +19,9 @@ func marchand(c *Character) {
 		case 2:
 			addInventory(c, "Potion de poison")
 		case 3:
+			addInventory(c, "Livre de sort : Boule de Feu")
+			spellBook(c)
+		case 4:
 			fmt.Println("Retour au menu principal.")
 			return
 		default:
