5b3f26e1b47517346c02fdda57d323c54e3b9505
Ivana Boycheva <ivana.boycheva@ynov.com>
2025-09-16 11:26:27 +0200

    Tache 9 - Intégration du menu principal, du marchand, des potions de vie et poison, et vérification de la mort

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ func main() {
 		fmt.Println("\n--- Menu principal ---")
 		fmt.Println("1. Afficher les informations du personnage")
 		fmt.Println("2. Accéder à l’inventaire")
-		fmt.Println("3. Quitter")
+		fmt.Println("3. Marchand")
+		fmt.Println("4. Quitter")
 
 		var choix int
 		fmt.Print("Votre choix : ")
@@ -23,6 +24,7 @@ func main() {
 				fmt.Println("\n--- Inventaire ---")
 				accessInventory(heros.Inventaire)
 				fmt.Println("1. Utiliser une potion")
+				fmt.Println("2. Utiliser une potion de poison")
 				fmt.Println("9. Retour")
 
 				var sub int
@@ -32,12 +34,16 @@ func main() {
 					break
 				} else if sub == 1 {
 					takePot(&heros)
+				} else if sub == 2 {
+					poisonPot(&heros)
+					removeInventory(&heros, "Potion de poison")
 				} else {
 					fmt.Println("Choix invalide.")
 				}
 			}
-
 		case 3:
+			marchand(&heros)
+		case 4:
 			fmt.Println("Au revoir !")
 			return
 		default:
--- shop.go
+++ shop.go
@@ -6,7 +6,8 @@ func marchand(c *Character) {
 	for {
 		fmt.Println("\n--- Marchand ---")
 		fmt.Println("1. Potion de vie")
-		fmt.Println("2. Retour au menu")
+		fmt.Println("2. Potion de poison")
+		fmt.Println("3. Retour au menu")
 
 		var choix int
 		fmt.Scan(&choix)
@@ -15,22 +16,10 @@ func marchand(c *Character) {
 		case 1:
 			addInventory(c, "Potion de vie")
 		case 2:
-			fmt.Println("Retour au menu principal.")
-			return
-		default:
-			fmt.Println("Choix invalide.")
-		}
-		fmt.Println("1. Potion de vie")
-		fmt.Println("2. Potion de poison")
-		fmt.Println("3. Retour au menu")
-
-		switch choix {
-		case 1:
-			addInventory(c, "Potion de vie")
-		case 2:
 			addInventory(c, "Potion de poison")
 		case 3:
 			fmt.Println("Retour au menu principal.")
+			return
 		default:
 			fmt.Println("Choix invalide.")
 		}
