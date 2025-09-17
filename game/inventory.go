95918bcf7cda7886decdbc018ab4e2c2b6fa124a
Ivana Boycheva <ivana.boycheva@ynov.com>
2025-09-17 22:13:39 +0200

    potion de poison  qui fait 10 de dégâts en 3 secondes

--- game/inventory.go
+++ game/inventory.go
@@ -1,6 +1,9 @@
 package game
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func AccessInventory(inventory []string) {
 	if len(inventory) == 0 {
@@ -53,6 +56,18 @@ func HasItem(inventory []string, item string) bool {
 	return false
 }
 
+func PoisonPot(c *Character) {
+	fmt.Println(c.Nom, "utilise une Potion de poison !")
+	for i := 1; i <= 3; i++ {
+		c.PVActuels -= 10
+		if c.PVActuels < 0 {
+			c.PVActuels = 0
+		}
+		fmt.Printf("Seconde %d : %s a %d/%d PV\n", i, c.Nom, c.PVActuels, c.PVMax)
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func AfficherInventaire(c *Character) {
 	for {
 		fmt.Println("\n--- Inventaire ---")
@@ -61,17 +76,27 @@ func AfficherInventaire(c *Character) {
 		if HasItem(c.Inventaire, "Potion") {
 			fmt.Println("1. Utiliser une potion de vie")
 		}
+		if HasItem(c.Inventaire, "Potion de poison") {
+			fmt.Println("2. Utiliser une potion de poison")
 		fmt.Println("0. Retour")
 
 		var sub int
 		fmt.Scan(&sub)
 
-		if sub == 2 {
-			break
-		} else if sub == 1 && HasItem(c.Inventaire, "Potion") {
-			TakePot(c)
-		} else {
-			fmt.Println("Choix invalide.")
-		}
+			switch sub {
+			case 1:
+				if HasItem(c.Inventaire, "Potion") {
+					TakePot(c)
+					RemoveInventory(c, "Potion")
+				}
+			case 2:
+				if HasItem(c.Inventaire, "Potion de poison") {
+					PoisonPot(c)
+					RemoveInventory(c, "Potion de poison")
+				}
+			case 0:
+				return
+			default:
+				fmt.Println("Choix invalide.")
 	}
 }
