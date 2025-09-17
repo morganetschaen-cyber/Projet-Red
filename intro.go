b87c0eb484f7d10348c3c33ef6ff65c0448c34dc
tmorgane <morgane.tschaen@ynov.com>
2025-09-17 14:58:02 +0200

    Ajout couleur rouge

--- intro.go
+++ intro.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func afficherIntro() bool {
+	fmt.Println("\033[31m"
 	fmt.Println("!     ____  __.                            ")
 	fmt.Println("!    |    |/ _|______ ___.__. ____   ____  ")
 	fmt.Println("!    |      < \\_  __ <   |  |/    \\ /    \\ ")
 	fmt.Println("!    |    |  \\ |  | \\/\\___  |   |  \\   |  \\")
 	fmt.Println("!    |____|__ \\|__|   / ____|___|  /___|  /")
 	fmt.Println("!            \\/       \\/         \\/     \\/ ")
+	fmt.Println("\033[0m")
 	return true
 }
