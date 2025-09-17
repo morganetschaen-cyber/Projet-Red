a2e6055ca12f122a288719ffaba8f4a59624a04f
tmorgane <morgane.tschaen@ynov.com>
2025-09-17 14:55:55 +0200

    Ajout du titre ASCII

---
+++ intro.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func afficherIntro() bool {
+	fmt.Println("!     ____  __.                            ")
+	fmt.Println("!    |    |/ _|______ ___.__. ____   ____  ")
+	fmt.Println("!    |      < \\_  __ <   |  |/    \\ /    \\ ")
+	fmt.Println("!    |    |  \\ |  | \\/\\___  |   |  \\   |  \\")
+	fmt.Println("!    |____|__ \\|__|   / ____|___|  /___|  /")
+	fmt.Println("!            \\/       \\/         \\/     \\/ ")
+	return true
+}
